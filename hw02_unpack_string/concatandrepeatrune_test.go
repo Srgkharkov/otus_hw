a6a55c5d5e57f6a44a76176de7aab9118e7a01cd
Sergei Kharkov <srgkharkov@gmail.com>
2022-07-27 19:49:06 +0600

    Added test case with empty inputstr

--- hw02_unpack_string/concatandrepeatrune_test.go
+++ hw02_unpack_string/concatandrepeatrune_test.go
@@ -16,6 +16,7 @@ func TestConcatandrepeatrune(t *testing.T) {
 		{inputstr: "a4bc2d5e", rune: 'a', count: 3, expected: "a4bc2d5eaaa"},
 		{inputstr: "a4bc2d5e", rune: 0, count: 3, expected: "a4bc2d5e"},
 		{inputstr: "a4bc2d5e", rune: 0, count: 100, expected: "a4bc2d5e"},
+		{inputstr: "", rune: '0', count: 10, expected: "0000000000"},
 	}
 
 	for _, tc := range tests {
