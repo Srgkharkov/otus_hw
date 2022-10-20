819c26fa3050d80864615f4ea641044eb2f1d5a4
Sergei Kharkov <srgkharkov@gmail.com>
2022-10-20 21:59:44 +0600

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,95 @@ package main
 
 import (
 	"errors"
+	"io"
+	"math"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrCanNotOpenFile        = errors.New("Can not open file")
+	ErrNoDataRead            = errors.New("No data for read")
+	ErrLimitNotCorrect       = errors.New("Limit is not correct")
+	ErrCannotCreateFile      = errors.New("Can not create file")
+	ErrCopyFile              = errors.New("Err copy")
+	ErrSeek                  = errors.New("Err Seek")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fsrc, err := os.OpenFile(fromPath, os.O_RDONLY, 0755)
+	if err != nil {
+		return ErrCanNotOpenFile
+	}
+	defer fsrc.Close()
+
+	fsrci, err := fsrc.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	size := fsrci.Size()
+
+	if offset < 0 {
+		offset += size
+	}
+
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit < 0 {
+		return ErrLimitNotCorrect
+	}
+
+	if limit == 0 {
+		limit = size - offset
+	}
+
+	if limit+offset > size {
+		limit = size - offset
+	}
+
+	if limit == 0 {
+		return ErrNoDataRead
+	}
+
+	_, err = fsrc.Seek(offset, 1)
+	if err != nil {
+		return ErrSeek
+	}
+	step := int64(math.Ceil(float64(limit) / 100))
+
+	fdst, err := os.Create(toPath)
+	if err != nil {
+		return ErrCannotCreateFile
+	}
+	defer fdst.Close()
+
+	// create bar
+	bar := pb.New64(limit).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
+	bar.ShowSpeed = true
+	bar.Start()
+
+	// create proxy writer
+	barWriter := bar.NewProxyWriter(fdst)
+
+	var i int64
+
+	for i = 0; i < limit; i += step {
+		if i+step > limit {
+			step = limit - i
+		}
+		_, err := io.CopyN(barWriter, fsrc, step)
+		if err != nil {
+			return ErrCopyFile
+		}
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,56 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Equal(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Successful copy", func(t *testing.T) {
+		file, err := os.CreateTemp("", "sample")
+		assert.Nil(t, err, "Can not CreateTemp")
+		defer os.Remove(file.Name())
+		file.WriteString("Sergei Kharkov 27.09.2022, 19:41 HW5 is completed!\nSergei Kharkov 27.09.2022, 19:36 HW5 is completed!")
+
+		destpath := "./temp/export.txt"
+
+		err = Copy(file.Name(), destpath, 0, 0)
+		assert.Nil(t, err, "Func Copy return error")
+		defer os.Remove(destpath)
+
+		srcbytes, err := os.ReadFile(file.Name())
+		assert.Nil(t, err, "Can not read source file")
+
+		destbytes, err := os.ReadFile(destpath)
+		assert.Nil(t, err, "Can not read destination file")
+
+		require.True(t, Equal(srcbytes, destbytes), "Not equal source file and destination file")
+	})
+
+	t.Run("Offset more than size file", func(t *testing.T) {
+		file, err := os.CreateTemp("", "sample")
+		defer os.Remove(file.Name())
+		file.WriteString("Sergei Kharkov 27.09.2022, 19:41 HW5 is completed!\nSergei Kharkov 27.09.2022, 19:36 HW5 is completed!")
+		destpath := "./temp/export.txt"
+		err = Copy(file.Name(), destpath, 1000, 0)
+		if err == nil {
+			os.Remove(destpath)
+		}
+		assert.Equal(t, ErrOffsetExceedsFileSize, err, "Expected ErrOffsetExceedsFileSize")
+	})
 }
