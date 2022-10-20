367c1865fa2fd983e6512776de9177b4b4d9110a
Sergei Kharkov <srgkharkov@gmail.com>
2022-10-20 22:10:26 +0600

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -22,35 +22,38 @@ func Equal(a, b []byte) bool {
 
 func TestCopy(t *testing.T) {
 	t.Run("Successful copy", func(t *testing.T) {
-		file, err := os.CreateTemp("", "sample")
+		fsrc, err := os.CreateTemp("", "sample")
 		assert.Nil(t, err, "Can not CreateTemp")
-		defer os.Remove(file.Name())
-		file.WriteString("Sergei Kharkov 27.09.2022, 19:41 HW5 is completed!\nSergei Kharkov 27.09.2022, 19:36 HW5 is completed!")
+		defer os.Remove(fsrc.Name())
+		fsrc.WriteString("Sergei Kharkov 27.09.2022, 19:41 HW5 is completed!\nSergei Kharkov 27.09.2022, 19:36 HW5 is completed!")
 
-		destpath := "./temp/export.txt"
+		fdst, err := os.CreateTemp("", "sample")
+		assert.Nil(t, err, "Can not CreateTemp")
+		defer os.Remove(fdst.Name())
 
-		err = Copy(file.Name(), destpath, 0, 0)
+		err = Copy(fsrc.Name(), fdst.Name(), 0, 0)
 		assert.Nil(t, err, "Func Copy return error")
-		defer os.Remove(destpath)
 
-		srcbytes, err := os.ReadFile(file.Name())
+		srcbytes, err := os.ReadFile(fsrc.Name())
 		assert.Nil(t, err, "Can not read source file")
 
-		destbytes, err := os.ReadFile(destpath)
+		destbytes, err := os.ReadFile(fdst.Name())
 		assert.Nil(t, err, "Can not read destination file")
 
 		require.True(t, Equal(srcbytes, destbytes), "Not equal source file and destination file")
 	})
 
 	t.Run("Offset more than size file", func(t *testing.T) {
-		file, err := os.CreateTemp("", "sample")
-		defer os.Remove(file.Name())
-		file.WriteString("Sergei Kharkov 27.09.2022, 19:41 HW5 is completed!\nSergei Kharkov 27.09.2022, 19:36 HW5 is completed!")
-		destpath := "./temp/export.txt"
-		err = Copy(file.Name(), destpath, 1000, 0)
-		if err == nil {
-			os.Remove(destpath)
-		}
+		fsrc, err := os.CreateTemp("", "sample")
+		defer os.Remove(fsrc.Name())
+		fsrc.WriteString("Sergei Kharkov 27.09.2022, 19:41 HW5 is completed!\nSergei Kharkov 27.09.2022, 19:36 HW5 is completed!")
+
+		fdst, err := os.CreateTemp("", "sample")
+		assert.Nil(t, err, "Can not CreateTemp")
+		defer os.Remove(fdst.Name())
+
+		err = Copy(fsrc.Name(), fdst.Name(), 1000, 0)
+
 		assert.Equal(t, ErrOffsetExceedsFileSize, err, "Expected ErrOffsetExceedsFileSize")
 	})
 }
