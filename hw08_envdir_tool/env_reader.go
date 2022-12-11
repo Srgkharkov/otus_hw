7146bd185466624a7a11ca7857c2bec25a6465f6
Sergei Kharkov <srgkharkov@gmail.com>
2022-12-11 22:01:49 +0600

    Complete

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +20,42 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment, 0)
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, entry := range entries {
+
+		if entry.IsDir() {
+			continue
+		}
+
+		file, err := os.Open(dir + "/" + entry.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		reader := bufio.NewReader(file)
+		line, _, err := reader.ReadLine()
+
+		file.Close()
+
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+
+		line = bytes.ReplaceAll(line, []byte("\x00"), []byte("\n"))
+
+		str := string(line)
+		str = strings.TrimRight(str, " :")
+
+		if len(str) > 0 {
+			env[entry.Name()] = EnvValue{Value: str, NeedRemove: false}
+		} else {
+			env[entry.Name()] = EnvValue{Value: "", NeedRemove: true}
+		}
+	}
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,53 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+func RunCmd(cmdstr []string, env Environment) (returnCode int) {
+	for key, mapvalue := range env {
+		if mapvalue.NeedRemove {
+			err := os.Unsetenv(key)
+			if err != nil {
+				return -1
+			}
+			continue
+		}
+		err := os.Setenv(key, mapvalue.Value)
+		if err != nil {
+			return -1
+		}
+
+	}
+	cmd := exec.Command(cmdstr[0], cmdstr[1:]...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+
+	if err != nil {
+		// try to get the exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			returnCode = ws.ExitStatus()
+		} else {
+			// This will happen (in OSX) if `name` is not available in $PATH,
+			// in this situation, exit code could not be get, and stderr will be
+			// empty string very likely, so we use the default fail code, and format err
+			// to string and set to stderr
+			log.Printf("Could not get exit code for failed program: %v", cmdstr)
+			returnCode = 1
+		}
+	} else {
+		// success, exitCode should be 0 if go is ok
+		ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		returnCode = ws.ExitStatus()
+	}
 	return
 }
